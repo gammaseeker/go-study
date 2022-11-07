85f21b2304bdf53c62cf47c8e0b4fcf818a771cb
Joey Jiemjitpolchai <joey.jiemjitpolchai@zendesk.com>
2022-11-07 10:00:41 -0500

    custom types

--- udemy/main.go
+++ udemy/main.go
@@ -6,8 +6,19 @@ func main() {
 	//var card string = "Ace of Spades"
 	card := newCard()
 	//card = "Five of Diamonds"
+	//cards := []string{"Ace of Diamonds", newCard()} // Slices are static
+	cards := deck{"Ace of Diamonds", newCard()} // Slices are static
+	cards = append(cards, "Six of Spades")      // append() returns a new slice
+
+	// i is index of this element in array
+	// c is current element we're iterating over
+	// range cards is collection we're looping over
+	for i, c := range cards {
+		fmt.Println(i, c)
+	}
 
 	fmt.Println(card)
+	fmt.Println(cards)
 }
 
 func newCard() string {
