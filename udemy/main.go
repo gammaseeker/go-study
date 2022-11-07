34aa27dcf983ffe21abebdac1a1f0cd1be914734
Joey Jiemjitpolchai <joey.jiemjitpolchai@zendesk.com>
2022-11-07 09:47:01 -0500

    function and return types

--- udemy/main.go
+++ udemy/main.go
@@ -4,10 +4,12 @@ import "fmt"
 
 func main() {
 	//var card string = "Ace of Spades"
-	card := "Ace of Spades"
+	card := newCard()
 	//card = "Five of Diamonds"
 
 	fmt.Println(card)
 }
 
-
+func newCard() string {
+	return "Five of Diamonds"
+}
