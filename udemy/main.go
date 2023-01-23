8eeedb54a7905577ada13cbeb92445654133e715
Joey Jiemjitpolchai <joey.jiemjitpolchai@zendesk.com>
2023-01-23 13:53:53 -0500

    implement deck receiver function

--- udemy/main.go
+++ udemy/main.go
@@ -10,13 +10,16 @@ func main() {
 	cards := deck{"Ace of Diamonds", newCard()} // Slices are static
 	cards = append(cards, "Six of Spades")      // append() returns a new slice
 
+	/*
 	// i is index of this element in array
 	// c is current element we're iterating over
 	// range cards is collection we're looping over
 	for i, c := range cards {
 		fmt.Println(i, c)
 	}
+	*/
 
+	cards.print()
 	fmt.Println(card)
 	fmt.Println(cards)
 }
